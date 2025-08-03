52d35a9c29b5177b4d71cbe05447600562ad12ab
Lina Khatsko <lina.khatsko@gmail.com>
2025-08-03 21:02:05 +0300

    fix psxabay

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -2,21 +2,24 @@ import axios from 'axios';
 
 const API_KEY = '51604264-079f506f95e2e038e0ca1ef24';
 const BASE_URL = 'https://pixabay.com/api/';
+const PER_PAGE = 15;
 
-export async function getImagesByQuery(query) {
+export async function getImagesByQuery(query, page = 1) {
   const params = {
     key: API_KEY,
     q: query,
     image_type: 'photo',
     orientation: 'horizontal',
     safesearch: true,
+    page,
+    per_page: PER_PAGE,
   };
-    console.log(query);
- return axios
-    .get(BASE_URL, { params})
-    .then((response) => response.data)
-    .catch(error => {
-      console.error("Error fetching images:", error);
-      throw new Error("Sorry, there are no images matching your search query. Please try again!")
-    });
-};
+
+  try {
+    const response = await axios.get(BASE_URL, { params });
+    return response.data;
+  } catch (error) {
+    console.error('❌ Error fetching images:', error.message);
+    throw new Error('Sorry, there are no images matching your search query. Please try again!');
+  }
+}
